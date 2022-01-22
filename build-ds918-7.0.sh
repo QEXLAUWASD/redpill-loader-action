560fc5c31216311c2f9de996fb77ffeda7790739
QEXLAUWASD <68603169+QEXLAUWASD@users.noreply.github.com>
2022-01-22 19:02:01 +0800

    Update build-ds918-7.0.sh

--- build-ds918-7.0.sh
+++ build-ds918-7.0.sh
@@ -29,5 +29,5 @@ cd redpill-load
 cp ${root}/user_config.DS918+.json ./user_config.json
 ./ext-manager.sh add https://raw.githubusercontent.com/jumkey/redpill-load/develop/redpill-acpid/rpext-index.json
 sudo ./build-loader.sh 'DS918+' '7.0.1-42218'
-mv images/redpill-DS918+_7.0-41890*.img ${root}/output/
+mv images/redpill-DS918+_7.0.1-42218*.img ${root}/output/
 cd ${root}
